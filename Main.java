b0549baefe1506d80361b7f67c7f3829a393f339
marsyuma <bintang.marsyuma@ui.ac.id>
2022-07-26 12:56:48 +0700

    first commit

---
+++ Main.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args){
+        System.out.print("Pick a number between 1 to 10: ");
+        Scanner scanner = new Scanner(System.in);
+        int input = scanner.nextInt();
+
+        if(input <= 5){
+            System.out.println("Hoki");
+        }
+        else{
+            System.out.println("Sial");
+        }
+    }
+}
