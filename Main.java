a490b407c5cf385d8526433f6a720d252e840bef
marsyuma <bintang.marsyuma@ui.ac.id>
2022-07-26 12:59:08 +0700

    second commit

--- Main.java
+++ Main.java
@@ -10,7 +10,7 @@ public class Main {
             System.out.println("Hoki");
         }
         else{
-            System.out.println("Sial");
+            System.out.println("Bapak kau");
         }
     }
 }
